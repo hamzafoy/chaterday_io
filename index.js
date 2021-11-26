6d030b3cec2a5fb4c08df0f9998b6091186f0fc8
Ross Foy <hamzaoflouisville@gmail.com>
2021-11-26 05:01:33 -0500

    added overflow-y scroll to enable chat messages to fill and become scrollable inside their ol parent.

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ $(function() {
 
     $('#user-input').on('submit', function(e) {
         e.preventDefault();
-        let nameData = $()
+        let chatbox = $('#chatbox')
+        let nameData = $('.user-input__name').val();
+        let msgData = $('.user-input__msg').val();
+        let submittedInput = `<li class="chatbox__text">${nameData}: ${msgData}</li>`;
+        chatbox.append(submittedInput);
+        $('.user-input__name').val('');
+        $('.user-input__msg').val('');
     })
 
 });
