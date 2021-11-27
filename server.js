5eb9850975907bbf7e6939d5015a1b53737ec9cc
Ross Foy <hamzaoflouisville@gmail.com>
2021-11-27 17:13:12 -0500

    installed express to handle server-side functionality.

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-//server code here
+const express = require('express');
+let app = express();
+const http = require('http');
+let server = http.Server(app);
+
+app.use(express.static('client'));
+
+server.listen(8080, function() {
+    console.log(`Spinning up the chat room!`)
+})
