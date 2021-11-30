79c3d9da369e73b81e79bc4e042679e0c480f401
Ross Foy <hamzaoflouisville@gmail.com>
2021-11-29 22:44:58 -0500

    add socket and emit messages between different clients over localhost.

--- client/index.js
+++ client/index.js
@@ -1,3 +1,5 @@
+let socket = io();
+
 $(function() {
 
     $('#user-input').on('submit', function(e) {
@@ -5,10 +7,15 @@ $(function() {
         let chatbox = $('#chatbox')
         let nameData = $('.user-input__name').val();
         let msgData = $('.user-input__msg').val();
+        socket.emit('message', msgData);
         let submittedInput = `<li class="chatbox__text">${nameData}: ${msgData}</li>`;
         chatbox.append(submittedInput);
         $('.user-input__name').val('');
         $('.user-input__msg').val('');
     })
 
+});
+
+socket.on('message', function(msg) {
+    $('<li class="chatbox__text">').text(msg).appendTo('#chatbox');
 });
--- server.js
+++ server.js
@@ -5,6 +5,13 @@ let server = http.Server(app);
 
 app.use(express.static('client'));
 
+let io = require('socket.io')(server);
+io.on('connection', function(socket) {
+    socket.on('message', function(msg) {
+        io.emit('message', msg);
+    })
+});
+
 server.listen(8080, function() {
     console.log(`Spinning up the chat room!`)
 })
