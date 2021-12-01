74ff9d7e9703a78770db8fe844c7adcdf5a18359
Ross Foy <hamzaoflouisville@gmail.com>
2021-12-01 04:59:13 -0500

    changed app to take user name only one time at first load using prompt()

--- client/index.js
+++ client/index.js
@@ -1,15 +1,18 @@
 let socket = io();
 
+
 $(function() {
+
+    let username = prompt('What is your name?');
 
     $('#user-input').on('submit', function(e) {
         e.preventDefault();
         let chatbox = $('#chatbox')
-        let nameData = $('.user-input__name').val();
+        //let nameData = $('.user-input__name').val();
         let msgData = $('.user-input__msg').val();
-        let submittedInput = `<li class="chatbox__text">${nameData}: ${msgData}</li>`;
+        let submittedInput = `<li class="chatbox__text">${username}: ${msgData}</li>`;
         socket.emit('message', submittedInput);
-        /* chatbox.append(submittedInput); */
+        //chatbox.append(submittedInput);
         $('.user-input__name').val('');
         $('.user-input__msg').val('');
     })
