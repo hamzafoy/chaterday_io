18eb3b50dedcdb1b4f44058b2bb4ceca0be374e7
Ross Foy <hamzaoflouisville@gmail.com>
2021-12-02 22:51:03 -0500

    removed unnecessary comments.

--- client/index.js
+++ client/index.js
@@ -5,10 +5,6 @@ $(function() {
 
     let username;
 
-    /* let username = prompt('What is your name?');
-    let chatMsgLabel = `<label for="user-input">${username}, enter your message here:</label>`;
-    $(chatMsgLabel).prependTo('#user-input'); */
-
     $('#username-input').on('submit', function() {
         let nameData = $('.user-input__name').val();
         username = nameData;
@@ -19,13 +15,9 @@ $(function() {
 
     $('#user-input').on('submit', function(e) {
         e.preventDefault();
-        let chatbox = $('#chatbox')
-        //let nameData = $('.user-input__name').val();
         let msgData = $('.user-input__msg').val();
         let submittedInput = `<li class="chatbox__text">${username}: ${msgData}</li>`;
         socket.emit('message', submittedInput);
-        //chatbox.append(submittedInput);
-        //$('.user-input__name').val('');
         $('.user-input__msg').val('');
     })
 
