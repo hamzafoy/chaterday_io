01374c0921162344c490e8378e99ae7edcec557f
Ross Foy <hamzaoflouisville@gmail.com>
2021-12-08 11:36:27 -0500

    tidying up codebase.

--- server.js
+++ server.js
@@ -39,25 +39,24 @@ io.on('connection', function(socket) {
     })
     socket.on('rawMessage', function(rawmsg) {
         socket.broadcast.emit('rawMessage', rawmsg);
-        console.log(rawmsg);
-        
-            const auth = new google.auth.GoogleAuth({
-                keyFile: "keys.json",
-                scopes: scope
-            });
-            const authClientObject =  auth.getClient();
-            const googleSheetsInstance = google.sheets({ version: "v4", auth: authClientObject });
-            const spreadsheetId = process.env.SPREADSHEET_ID;
-             googleSheetsInstance.spreadsheets.values.append({
-                auth,
-                spreadsheetId,
-                range: "Sheet1!A:A",
-                valueInputOption: 'RAW',
-                insertDataOption: 'INSERT_ROWS',
-                resource: {
-                    values: [[rawmsg]]
-                }
-            });
+        //console.log(rawmsg);
+        const auth = new google.auth.GoogleAuth({
+            keyFile: "keys.json",
+            scopes: scope
+        });
+        const authClientObject =  auth.getClient();
+        const googleSheetsInstance = google.sheets({ version: "v4", auth: authClientObject });
+        const spreadsheetId = process.env.SPREADSHEET_ID;
+            googleSheetsInstance.spreadsheets.values.append({
+            auth,
+            spreadsheetId,
+            range: "Sheet1!A:A",
+            valueInputOption: 'RAW',
+            insertDataOption: 'INSERT_ROWS',
+            resource: {
+                values: [[rawmsg]]
+            }
+        });
         })
 });
 
