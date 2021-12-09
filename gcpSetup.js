f3d656802275dedfd897d91fdd352b99cc424eae
Ross Foy <hamzaoflouisville@gmail.com>
2021-12-09 02:58:31 -0500

    minor fix.

--- gcpSetup.js
+++ gcpSetup.js
@@ -1,2 +1,2 @@
 let fs=require('fs');
-fs.writeFile(process.env.GOOGLE_KEYS_FILE, process.env.GOOGLE.KEYS, (err) => {});
+fs.writeFile(process.env.GOOGLE_KEYS_FILE, process.env.GOOGLE_KEYS, (err) => {});
